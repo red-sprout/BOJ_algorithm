de900f7d261b06cbf0cb81451da5132daeaa820a
red-sprout <jjw05015@gmail.com>
2023-09-18 00:17:10 +0900

    Lv.0

--- Lv.0/Math/BOJ17425.py
+++ Lv.0/Math/BOJ17425.py
@@ -1,8 +1,23 @@
-MAX = 1000000
+MAX = 1000001
+arr = [0 for i in range(MAX+1)]
+for i in range(1, MAX+1):
+    j = 1
+    while i * j <= MAX:
+        arr[i * j] += i
+        j += 1
+for i in range(1, MAX+1):
+    arr[i] = arr[i-1] + arr[i]
+
 T = int(input())
+ans = []
 for i in range(T):
-    g = 0
-    N = int(input())
-    for i in range(1, N + 1):
-        g += i * (N // i)
-    print(g)
+    S = int(input())
+    ans.append(str(arr[S])+'\n')
+print(''.join(ans))
+
+# BOJ17427.py
+# N = int(input())
+# ans = 0
+# for i in range(1, N+1):
+#     ans += i * (N // i)
+# print(ans)
