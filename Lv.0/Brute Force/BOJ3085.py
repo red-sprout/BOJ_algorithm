0109444cba0f3357f7b700a8ffd515c744243ebf
red-sprout <jjw05015@gmail.com>
2023-09-22 13:02:10 +0900

    Lv.0

--- Lv.0/Brute Force/BOJ3085.py
+++ Lv.0/Brute Force/BOJ3085.py
@@ -1,4 +1,3 @@
-#진행중
 import sys
 
 arr = []
@@ -7,58 +6,61 @@ for _ in range(N):
     arr.append(list(input()))
 
 check = []
+check_part = []
 dummy = 1
 
 for i in range(N):
     for j in range(N - 1):
-        temp = arr[i][j]
-        arr[i][j] = arr[i][j + 1]
-        arr[i][j + 1] = temp
+        arr[i][j], arr[i][j + 1] = arr[i][j+1], arr[i][j]
 
         for l in range(N):
             for m in range(N - 1):
                 if arr[l][m] == arr[l][m + 1]:
                     dummy += 1
-            check.append(dummy)
-
-        dummy = 1
+                else:
+                    dummy = 1
+                check.append(dummy)
+            check_part = []
+            dummy = 1
 
         for l in range(N):
             for m in range(N - 1):
                 if arr[m][l] == arr[m + 1][l]:
                     dummy += 1
-            check.append(dummy)
-        
-        dummy = 1
+                else:
+                    dummy = 1
+                check.append(dummy)
+            check_part = []
+            dummy = 1
 
-        temp = arr[i][j]
-        arr[i][j] = arr[i][j + 1]
-        arr[i][j + 1] = temp
+        arr[i][j], arr[i][j + 1] = arr[i][j+1], arr[i][j]
 
 for i in range(N):
     for j in range(N - 1):
-        temp = arr[j][i]
-        arr[j][i] = arr[j + 1][i]
-        arr[j + 1][i] = temp
+        arr[j][i] , arr[j + 1][i] = arr[j + 1][i] , arr[j][i]
 
         for l in range(N):
             for m in range(N - 1):
                 if arr[l][m] == arr[l][m + 1]:
                     dummy += 1
-            check.append(dummy)
-
-        dummy = 1
+                else:
+                    dummy = 1
+                check_part.append(dummy)
+            check.append(max(check_part))
+            check_part = []
+            dummy = 1
 
         for l in range(N):
             for m in range(N - 1):
                 if arr[m][l] == arr[m + 1][l]:
                     dummy += 1
-            check.append(dummy)
-        
-        dummy = 1
+                else:
+                    dummy = 1
+                check_part.append(dummy)
+            check.append(max(check_part))
+            check_part = []
+            dummy = 1
 
-        temp = arr[j][i]
-        arr[j][i] = arr[j + 1][i]
-        arr[j + 1][i] = temp
+        arr[j][i] , arr[j + 1][i] = arr[j + 1][i] , arr[j][i]
 
 print(max(check))
