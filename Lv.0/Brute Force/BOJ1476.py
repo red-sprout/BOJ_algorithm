97e9daa6942809a8ce5263a181bde10d8218fa8d
red-sprout <jjw05015@gmail.com>
2023-09-22 15:18:54 +0900

    Lv.0

--- Lv.0/Brute Force/BOJ1476.py
+++ Lv.0/Brute Force/BOJ1476.py
@@ -1,4 +1,3 @@
-#진행중
 def calSn(n, y):
     s = 0
     while True:
@@ -8,7 +7,17 @@ def calSn(n, y):
 year = list(map(int, input().split()))
 E, S, M = 15, 28, 19
 m = E * S * M
-n1, n2, n3 = S * M, E * M, E * S
-s1, s2, s3 = calSn(n1, E), calSn(n2, S), calSn(n3, M)
+y = 1
+while True:
+    if y % E == year[0] % E:
+        if y % S == year[1] % S:
+            if y % M == year[2] % M:
+                break
+    y += 1
+print(y)
 
-print((year[0] * n1 * s1 + year[1] * n2 * s2 + year[2] * n3 * n3) % m)
+# 중국인의 나머지 정리 시도
+# n1, n2, n3 = S * M, E * M, E * S
+# s1, s2, s3 = calSn(n1, E), calSn(n2, S), calSn(n3, M)
+
+# print((year[0] * n1 * s1 + year[1] * n2 * s2 + year[2] * n3 * n3) % m)
