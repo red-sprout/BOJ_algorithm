207348d62233ef1dcb69f368437a26f4baf25d0e
red-sprout <jjw05015@gmail.com>
2023-09-28 14:57:44 +0900

    Lv.0

--- Lv.0/Dynamic Programming/BOJ15990.py
+++ Lv.0/Dynamic Programming/BOJ15990.py
@@ -1,6 +1,14 @@
-#진행중
 CONST = 1000000009
+MAX = 100000
+
 T = int(input())
+end1, end2, end3 = [0, 1, 0, 1], [0, 0, 1, 1], [0, 0, 0, 1]
+
+for i in range(4, MAX + 1):
+    end1.append((end2[i-1] + end3[i-1]) % CONST)
+    end2.append((end1[i-2] + end3[i-2]) % CONST)
+    end3.append((end1[i-3] + end2[i-3]) % CONST)
+
 for _ in range(T):
-    cand = [0, 1, 1, 3, 3, 4]
     N = int(input())
+    print((end1[N] + end2[N] + end3[N]) % CONST)
