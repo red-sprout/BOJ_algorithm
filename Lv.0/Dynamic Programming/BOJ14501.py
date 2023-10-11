4fbfd5f277a52c3ac1b2098efbed1cf0e1deb279
red-sprout <jjw05015@gmail.com>
2023-10-11 15:53:37 +0900

    진행중

--- Lv.0/Dynamic Programming/BOJ14501.py
+++ Lv.0/Dynamic Programming/BOJ14501.py
@@ -4,16 +4,3 @@ TP = []
 for _ in range(N):
     tmp = list(map(int,input().split()))
     TP.append(tmp)
-
-price = 0
-def dfs(idx):
-    global price
-    tmp = 0
-    for i in range(idx, N):
-        idx = i + TP[i][0]
-        if idx > N:
-            continue
-        tmp = max(tmp, TP[i][0])
-    return price
-
-print(dfs(0))
