60096608d520cf58de926f1251a89a5e3922c854
red-sprout <jjw05015@gmail.com>
2023-10-24 14:41:58 +0900

    Lv.0

--- Lv.0/Dynamic Programming/BOJ13398.py
+++ Lv.0/Dynamic Programming/BOJ13398.py
@@ -1,24 +1,7 @@
-# 진행중
-import copy
-
 n = int(input())
 arr = list(map(int,input().split()))
-minus = []
-ans = 0
-for i in range(n):
-    if arr[i] < 0:
-        minus.append(i)
-if len(minus) == 0:
-    print(sum(arr))
-else:
-    for j in minus:
-        DP = copy.deepcopy(arr)
-        for i in range(1,n):
-            if i == j:
-                DP[i] = DP[i-1]
-                continue
-            DP[i] = max(arr[i], arr[i] + DP[i-1])
-        tmp = max(DP)
-        if ans < tmp:
-            ans = tmp
-print(ans)
+DP = [[i for i in arr] for j in range(2)]
+for i in range(1, n):
+    DP[0][i] = max(DP[0][i], DP[0][i-1] + DP[0][i])
+    DP[1][i] = max(DP[0][i-1], DP[1][i-1] + arr[i])
+print(max(max(DP[0]), max(DP[1])))
