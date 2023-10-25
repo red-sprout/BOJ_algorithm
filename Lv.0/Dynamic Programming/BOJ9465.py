fdf3c5fd5a7b0afd12c641e651dafb5ca24a11ca
red-sprout <jjw05015@gmail.com>
2023-10-25 21:16:17 +0900

    Lv.0

--- Lv.0/Dynamic Programming/BOJ9465.py
+++ Lv.0/Dynamic Programming/BOJ9465.py
@@ -6,7 +6,8 @@ for _ in range(T):
     row = [row_1, row_2]
     DP = [[0, 0] for i in range(n)]
     DP[0][0], DP[0][1] = row[0][0], row[1][0]
-    DP[1][0], DP[1][1] = DP[0][1] + row[0][1], DP[0][0] + row[1][1]
+    if n > 1:
+        DP[1][0], DP[1][1] = DP[0][1] + row[0][1], DP[0][0] + row[1][1]
     for i in range(2, n):
         DP[i][0] = max(DP[i-1][1] + row[0][i], max(DP[i-2]) + row[0][i])
         DP[i][1] = max(DP[i-1][0] + row[1][i], max(DP[i-2]) + row[1][i])
